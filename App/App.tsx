7d5414cf1c58e1f1346d61da19b049effd7df1c8
Mohit Kumar <mohitk@geekyants.com>
2021-05-05 22:16:35 +0530

    feat(App.tsx) : Styles updated

--- App/App.tsx
+++ App/App.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import type {ReactNode} from 'react';
-import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+  StyleSheet,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => ReactNode = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Produce</Text>
+      <View style={styles.parentContainer}>
+        <Text>Produce</Text>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  parentContainer: {flex: 1},
+});
 
 export default App;
