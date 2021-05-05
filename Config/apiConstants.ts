c22735192820583b48c56b05ab9af633e22be16b
Mohit Kumar <mohitk@geekyants.com>
2021-05-05 22:26:25 +0530

    feat(Configs) : Configs (api constants & url endpoints) updated

--- Config/apiConstants.ts
+++ Config/apiConstants.ts
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  BASE_HEADER: {
+    'Content-Type': 'application/json',
+  },
+  TIMEOUT: 20000,
+  API_END_POINT_PREFIX: '/api',
+  API_VERSION: '/v1',
+  API_END_POINTS: {},
+};
---
+++ Config/urlEndPoints.ts
@@ -0,0 +1,9 @@
+/*
+To get urlEndPoint using current environment
+*/
+
+const developmentApiUrl = '';
+
+export const url = {
+  developmentApiUrl: developmentApiUrl,
+};
