9f378ab7a96084d8b59f74f6d70c38ef1c58d9c0
Mohit Kumar <mohitk@geekyants.com>
2021-05-07 00:35:14 +0530

    feat(Theme, packages, Landing) : Added custom theme values, responsive sylings on style parameters, updated Landing screen and related Utils

--- App/Containers/Landing/index.tsx
+++ App/Containers/Landing/index.tsx
@@ -3,12 +3,21 @@ import {View, Text} from 'react-native';
 import {styles} from './styles';
 import {ILandingProps} from 'App/Interfaces';
 import {LocaleString} from 'App/Utils';
+import {MetricsSizes} from 'App/Theme';
 
 export const Landing = (props: ILandingProps) => {
   console.log('Landing : props ::', props);
   return (
     <View style={styles.parentContainer}>
       <Text>{LocaleString.LandingScreen}</Text>
+      <View
+        style={{
+          height: MetricsSizes.extraLargeHumongous,
+          width: MetricsSizes.extraLargeHumongous,
+          backgroundColor: 'red',
+          marginTop: 40,
+        }}
+      />
     </View>
   );
 };
--- App/Theme/index.ts
+++ App/Theme/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from './Colors';
+export * from './Metrics';
