41885b56207243c355015ebee7825d826324e2d8
Mohit Kumar <mohitk@geekyants.com>
2021-05-12 00:21:42 +0530

    feat(Services) : Parse in RN configures, login and logout API added

--- App/Services/Api/auth.ts
+++ App/Services/Api/auth.ts
@@ -1,9 +1,16 @@
 import Parse from '../parse';
-import cloudFunction from '../parse';
 
-export const loginUser = async (
-  email: string,
-  password: string
-): Promise<any> => {
+export const login = async (email: string, password: string): Promise<any> => {
   return Parse.User.logIn(email, password);
+};
+
+export const getLoggedInUserInfo = async (): Promise<any> => {
+  const userInfoResponse = await Parse.User.currentAsync().then((user: any) =>
+    JSON.parse(JSON.stringify(user)),
+  );
+  return userInfoResponse;
+};
+
+export const logout = async (): Promise<any> => {
+  return Parse.User.logOut();
 };
--- App/Services/parse.ts
+++ App/Services/parse.ts
@@ -5,19 +5,22 @@ Parse : Powerful JavaScript SDK for Parse Server backend from Client App
 let Parse: any;
 Parse = require('parse/react-native.js');
 
+const AsyncStorage = require('react-native').AsyncStorage;
+Parse.setAsyncStorage(AsyncStorage);
+
 Parse.initialize(ApiConstant.ParseAppId, ApiConstant.ParseJsKey);
 Parse.serverURL = ApiBaseUrl.development;
 
 export async function cloudFunction(
   funName: string,
-  params: Record<string, any>
+  params: Record<string, any>,
 ): Promise<any> {
   try {
     const response = await Parse.Cloud.run(funName, params);
     return response;
   } catch (error) {
     console.log(error);
-    throw error.message || error.toString() || error;
+    // throw error.message || error.toString() || error;
   }
 }
 
