8e7d44d7b9118f055a97fa7850d542621dfc4f66
Mohit Kumar <mohitk@geekyants.com>
2021-05-15 18:41:27 +0530

    feat(Cart, Profile, CategoryItemList) : Skeleton Screen for Cart, Category and profile added, related component and utils updated

--- App/Containers/Cart/index.tsx
+++ App/Containers/Cart/index.tsx
@@ -3,16 +3,19 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { styles } from './styles';
 import { appStyles } from 'App/Utils/globalStyles';
 import { Header } from 'App/Components';
-import { IProfileProps } from 'App/Interfaces';
+import { ICartProps } from 'App/Interfaces';
 import { openDrawer } from 'App/Navigators/navigationServices';
 
-export const Profile = (props: IProfileProps) => {
+export const Cart = (props: ICartProps) => {
   return (
     <View style={appStyles.parentContainerNoMargin}>
       <SafeAreaView style={appStyles.safeAreaTopStyleWithColor} />
-      <Header title="Settings" handleLeftButtonClick={() => openDrawer()} />
+      <Header
+        title="My order"
+        handleLeftButtonClick={() => props.navigation.goBack()}
+      />
       <View style={styles.mainContainer}>
-        <Text>Profile Screen</Text>
+        <Text>Cart Screen</Text>
       </View>
     </View>
   );
